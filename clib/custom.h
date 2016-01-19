ccd6202108bc8eacb7db80e9bed6a9029b0a5c3e
Matthew Hsu <gears88@gmail.com>
2016-01-19 17:10:51 -0500

    Begin convertinng L3GD20 to work with C.

---
+++ clib/custom.h
@@ -0,0 +1,16 @@
+#ifndef _CUSTOM_H
+#define _CUSTOM_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include <unistd.h>
+
+#include <wiringPiI2C.h>
+
+typedef unsigned char byte;
+
+
+#endif
